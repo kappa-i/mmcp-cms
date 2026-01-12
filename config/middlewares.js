5cd8433c01d1e28ea372ee2ed3f5735aba492598
kappa-i <gaby.kappai@gmail.com>
2026-01-12 17:38:47 +0100

    .

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,12 +7,12 @@ module.exports = [
     config: {
       origin: [
         'https://kappa-i.github.io',
-        'http://localhost:5500',
+        'http://localhost:5500',       // Frontend local (Live Server)
         'http://127.0.0.1:5500',
-        'http://localhost:1337',      // Admin local
-        'http://127.0.0.1:1337',      // Admin local (variante)
-        'http://localhost:*',         // Tous les ports localhost
-        'http://127.0.0.1:*'          // Tous les ports 127.0.0.1
+        'http://localhost:*',          // Tous les ports localhost
+        'http://127.0.0.1:*',
+        'https://mmcp.ch',             // Site en production
+        'https://www.mmcp.ch'
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
