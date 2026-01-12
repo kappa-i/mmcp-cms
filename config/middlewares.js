ed9685b994a029e1831cd328340eae96ca80e1a5
kappa-i <gaby.kappai@gmail.com>
2026-01-12 17:35:17 +0100

    .

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,9 +6,13 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: [
-        'https://kappa-i.github.io', // <--- REMPLACE PAR TON URL GITHUB PAGES
-        'http://localhost:3000',       // Pour le Live Server VS Code
-        'http://127.0.0.1:3000'        // Variante du Live Server
+        'https://kappa-i.github.io',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'http://localhost:1337',      // Admin local
+        'http://127.0.0.1:1337',      // Admin local (variante)
+        'http://localhost:*',         // Tous les ports localhost
+        'http://127.0.0.1:*'          // Tous les ports 127.0.0.1
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
